49193473ba712e0b4de174493853d665c9d1899c
ramtin <ramtin.hrezaei@gmail.com>
2020-10-04 23:58:49 +0330

    change client

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import OtherPage from './OtherPage'
-import Fib from './Fib'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Fib Calculator version 2</h1>
-            <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
-          </header>
-          <div>
-            <Route exact path="/" component={Fib} />
-            <Route path="/otherpage" component={OtherPage} />
-          </div>
-        </div>
-      </Router>
-    )
-  }
+function App() {
+  return (
+    <Router>
+          <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Fib Calculator version 2
+        </p>
+        <Link to="/">Home</Link>
+        <Link to="/otherpage">Other Page</Link>
+      </header>
+    </div>
+     <Route exact path="/" component={Fib} />
+     <Route path="/otherpage" component={OtherPage} />
+    </Router>
+  );
 }
 
-export default App
+export default App;
